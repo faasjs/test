5527201f720a44da7a0faa503cfd9952cc343e58
Ben <ben@zfben.com>
2019-06-05 20:14:06 +0800

    fix test

--- src/__tests__/http.test.ts
+++ src/__tests__/http.test.ts
@@ -1,6 +1,6 @@
 import { FuncWarpper } from '../../src/index';
 
-const handler = new FuncWarpper(require.resolve('./funcs/http.flow')).handler();
+const handler = new FuncWarpper(require.resolve('./funcs/http.func')).handler();
 
 test('http', async function () {
   const res = await handler({}, {});
